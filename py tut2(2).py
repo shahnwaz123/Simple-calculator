44f52466d52b32d164a843248f5aa34f578eb5c0
shahnwaz khan <88053176+shahnwaz123@users.noreply.github.com>
2021-08-12 14:20:06 +0530

    Add files via upload

---
+++ py tut2(2).py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # how to take user input by using input() funtion.
+
+# In[ ]:
+
+
+print("enter your number")
+inpnum=input()
+print("your number is", int(inpnum)+10)
+
+
+# In[3]:
+
+
+# we need to convert it into integer because as defalut input data is stored as str so we cant add a int value into str.
+
+
+# # Lets try to build a calculator
+
+# In[ ]:
+
+
+# this calculator used to multiply the value.
+
+
+# In[ ]:
+
+
+print("enter your 1st number")
+num1=input()
+print("enter your 2nd number")
+num2=input()
+print("sum of these number is",int(num1)*int(num2))
+
+
+# In[ ]:
+
+
+
+
